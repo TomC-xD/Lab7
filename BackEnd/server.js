046aa8085811a7f2aa4f407fbb5f4a5ad01e0144
TomC-xD <G00410308@atu.ie>
2024-11-15 15:26:12 +0000

    added movie stored and respond with JSON

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -16,38 +16,53 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/api/movies', (req, res) => {
-    const movies = [
-        {
-          "Title": "Avengers: Infinity War (server)",
-          "Year": "2018",
-          "imdbID": "tt4154756",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-          "Title": "Captain America: Civil War (server)",
-          "Year": "2016",
-          "imdbID": "tt3498820",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        },
-        {
-          "Title": "World War Z (server)",
-          "Year": "2013",
-          "imdbID": "tt0816711",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        }
-      ];
-    res.status(200).json({movies})
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:admin@cluster0.9dmwv.mongodb.net/DB24');
+
+const movieSchema = new mongoose.Schema({
+  title:String,
+  year:String,
+  poster:String
 });
 
-app.post('/api/movies',(req, res)=>{
-    console.log(req.body.title);
-    res.send("Movie Added!");
-})
+const movieModel = new mongoose.model('myMovies', movieSchema);
+
+app.get('/api/movies', async (req, res) => { //Movie stored
+    const movies = await movieModel.find({});
+    res.status(200).json({movies}) //Respond with the JSON
+  });
+  
+  app.post('/api/movies', async (req, res)=>{
+    
+    const { title, year, poster } = req.body;
+ 
+  const newMovie = new Movie({ title, year, poster });
+  await newMovie.save();
+  
+  res.status(201).json({ message: 'Movie created successfully', movie: newMovie });
+  })
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
+  });
+  // {
+  //   "Title": "Avengers: Infinity War (server)",
+  //   "Year": "2018",
+  //   "imdbID": "tt4154756",
+  //   "Type": "movie",
+  //   "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+  // },
+  // {
+  //   "Title": "Captain America: Civil War (server)",
+  //   "Year": "2016",
+  //   "imdbID": "tt3498820",
+  //   "Type": "movie",
+  //   "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+  // },
+  // {
+  //   "Title": "World War Z (server)",
+  //   "Year": "2013",
+  //   "imdbID": "tt0816711",
+  //   "Type": "movie",
+  //   "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+  // }
